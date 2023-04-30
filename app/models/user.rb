fc03c1cba0851cb75e0341e60fe03c0b8f0b0119
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-04-30 05:54:27 +0000

    [update]応用課題4

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   has_many :favorites,dependent: :destroy
   has_many :book_comments,dependent: :destroy
   has_many :follower,class_name: "Relationship",foreign_key: "follower_id",dependent: :destroy
-  has_many :follow_ed, through: :follower,source: :followed
   has_many :followed,class_name: "Relationship",foreign_key: "followed_id",dependent: :destroy
+  has_many :follow_ed, through: :follower,source: :followed
   has_many :follow_er, through: :followed,source: :follower
   has_one_attached :profile_image
 
