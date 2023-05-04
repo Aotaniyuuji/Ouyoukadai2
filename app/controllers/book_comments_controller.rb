9cf2fc894c40909b6278988b2ddde457516b1d75
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-05-04 06:07:13 +0000

    [update]応用課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,12 +4,10 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(post_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to book_path(book)
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_to request.referer
   end
 
   private
